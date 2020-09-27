1f613312ca44d367899c3b81e7655eec6aaa9295
Fabio Seixas <flscosta94@gmail.com>
2020-09-26 23:16:45 -0300

    createTransaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,46 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    category,
+    type,
+    title,
+    value,
+  }: RequestDTO): Promise<Transaction> {
+    const transactionsRepo = getRepository(Transaction);
+    const categoriesRepo = getRepository(Category);
+
+    const categoryExists = await categoriesRepo.findOne({ title: category });
+
+    if (!categoryExists) {
+      const newCategory = categoriesRepo.create({
+        title: category,
+      });
+
+      await categoriesRepo.save(newCategory);
+    }
+
+    const newTransaction = transactionsRepo.create({
+      title,
+      type,
+      value,
+      category,
+    });
+
+    await transactionsRepo.save(newTransaction);
+
+    return newTransaction;
   }
 }
 
